0324f3793efcae3e73f173ad5e499861108192e2
noella2001 <mogboella@gmail.com>
2025-05-15 12:12:18 +0100

    update hero bar

--- layouts/AppFooter.vue
+++ layouts/AppFooter.vue
@@ -1,14 +1,9 @@
 
 <template>
-  <v-row justify="center" no-gutters>
-    <v-btn class="mx-2" rounded="xl" variant="plain" href="/">HOME</v-btn>
-    <v-btn class="mx-2" rounded="xl" variant="plain" href="/about">ABOUT</v-btn>
-    <v-btn class="mx-2" rounded="xl" variant="plain" href="/blog"
-      >ARTICLES</v-btn
-    >
-    <!-- <v-btn class="mx-2" rounded="xl" variant="plain" @click="socials.downloadResume()">RESUMÉ</v-btn> -->
-    <v-col class="text-center mt-4" cols="12">
-      {{ new Date().getFullYear() }} — <strong>Nkem's Tech Teachings</strong>
+  <v-row justify="start" no-gutters>
+    <v-col class="ml-3 mt-2 mb-2" cols="12">
+      © {{ new Date().getFullYear() }} Nkem E Mogbo Production. All rights
+      reserved.
     </v-col>
   </v-row>
 </template>
--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -8,6 +8,22 @@ export default defineNuxtPlugin(nuxtApp => {
     ssr: true,
     components,
     directives,
+    theme: {
+      defaultTheme: 'myCustomTheme',
+      themes: {
+        myCustomTheme: {
+          dark: false,
+          colors: {
+            primary: '#4F46E5',        
+            secondary: '#E0F2FE',      
+            background: '#ffffff',
+            surface: '#f5f7fa',        
+            'on-primary': '#ffffff',
+            'on-secondary': '#1E3A8A', 
+          },
+        },
+      },
+    },
   })
 
   nuxtApp.vueApp.use(vuetify)
